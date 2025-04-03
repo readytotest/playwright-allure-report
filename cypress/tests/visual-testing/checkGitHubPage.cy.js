53d803a95b3de884c7c70bd1fd7274dd79d8e63b
readytotest <readytotest@users.noreply.github.com>
2025-04-03 14:21:47 -0700

    deploy: readytotest/readytotest.github.io@3c46d3e0d94678771f7084165e639f98552a4b5f

--- cypress/tests/visual-testing/checkGitHubPage.cy.js
+++
@@ -1,6 +0,0 @@
-describe("My GitHub Site Visual Test", () => {
-  it("should compare screenshot of the entire page", () => {
-    cy.visit("https://readytotest.github.io");
-    cy.compareSnapshot("my-github-site");
-  });
-});
